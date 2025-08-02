736a9520e8e44c87d738e5c215a8615c3033f59c
engsanjid <mdsanjidi36@gmail.com>
2025-08-02 18:48:47 +0600

    added break

---
+++ loop-tasks/break/century.js
@@ -0,0 +1,16 @@
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+let num;
+for(num=1;num<=200;num++)
+{
+     console.log(num);
+    if(num==100)
+    {
+        
+        break ;
+    }
+   
+}
